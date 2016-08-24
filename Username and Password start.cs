35018e592e153bee2cf800d382ede94b1723bb9e
Unstupid <thomasfrith@icloud.com>
2016-08-24 08:35:17 +0100

    Add files via upload

---
+++ Username and Password start.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class LogOnScreen : Form
+    {
+        public LogOnScreen()
+        {
+            InitializeComponent();
+            LogOnSuccessShow.Text = ("Welcome to the Allotment System");
+        }
+
+        private void PasswordBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LogOnButton_Click(object sender, EventArgs e)
+        {
+            string UserName;
+            UserName = UsernameBox.Text;
+            string Password;
+            Password = PasswordBox.Text;
+            if (UserName == "Username" && Password == "Password")
+            {
+                MessageBox.Show("Logging On...", "LogOnSuccessful", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk);
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect User/Password Combination", "LogOnFailure", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                LogOnSuccessShow.Text = ("Try to log on again - check the case of your password and username.");
+            }
+        }
+    }
+}
